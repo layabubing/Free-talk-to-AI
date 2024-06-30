a8a2e681d06e8b29587a625cc515934af5cbaa3b
downloading <119787894+layabubing@users.noreply.github.com>
2024-06-30 15:58:06 +0800

    Update main.py
    Fix a bug

--- main/main.py
+++ main/main.py
@@ -18,6 +18,6 @@ answer = answer.choices[0].message.content
 
 play_audio = Thread(target = create_audio.play_audio, args=(answer, ))
 play_audio.start()
-webview.create_window('Answer', html=functions.cut_answer(answer), width=1000, height=1000)
+webview.create_window('Answer', html=cut_answer.cut_answer(answer), width=1000, height=1000)
 webview.start()
 play_audio.join()
